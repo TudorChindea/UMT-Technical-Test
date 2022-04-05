6eef1e81e800a043f866ddd1920ec4f363698910
TudorChindea <56337986+TudorChindea@users.noreply.github.com>
2022-04-05 14:45:56 +0300

    Add files via upload

--- UMT_Solution/UMT_Test.py
+++ UMT_Solution/UMT_Test.py
@@ -14,7 +14,7 @@ def minimumChanges(password):
     currentRepeatingChar = 0
     lastChr = 'a'
     #go through the entire password and check all characters if they are either, a number, lower case letter, uppercase letter or a special character
-    for c in range(0,password_length):
+    for c in range(password_length-1):
         if c == 0: 
             lastChr = password[c]
             currentRepeatingChar +=1
@@ -32,7 +32,7 @@ def minimumChanges(password):
     #we return the maximum number between the:
     #       1: difference between the minimum password length and the number of characters 
     #       2: difference between the number of conditions that need to be met and the ones that are already met
-    #       3: the number of times more than 3 characters appear toghether
+    #       3: the number of times more than 3 characters appear together
     max1 = max(6-password_length,password_length-20)
     return max(max1, hasNumber + hasSpecial + hasLower + hasUpper + maxRepeatingChars)
 #read the number of characters in the password and the pa
