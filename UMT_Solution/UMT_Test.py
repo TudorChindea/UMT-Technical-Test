a1e4bf2bb5a001870d3667f4cbf96fcbc8fd3ad6
TudorChindea <56337986+TudorChindea@users.noreply.github.com>
2022-04-05 14:32:06 +0300

    Add files via upload

--- UMT_Solution/UMT_Test.py
+++ UMT_Solution/UMT_Test.py
@@ -6,10 +6,10 @@ def minimumChanges(password):
     upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     special_char = "!@#$%^&*()-+"
     password_length = len(password)
-    hasNumber = 0
-    hasLower = 0
-    hasUpper = 0
-    hasSpecial = 0
+    hasNumber = 1
+    hasLower = 1
+    hasUpper = 1
+    hasSpecial = 1
     maxRepeatingChars = 0
     currentRepeatingChar = 0
     lastChr = 'a'
@@ -21,21 +21,20 @@ def minimumChanges(password):
         elif password[c] == lastChr:
             currentRepeatingChar +=1
         else:
-            if currentRepeatingChar>3:
+            if currentRepeatingChar>= 3:
                 maxRepeatingChars +=1
-                currentRepeatingChar = 0
-                lastChr = password[c]
-        if password[c] in numbers: hasNumber = 1
-        elif password[c] in lower_case: hasLower = 1
-        elif password[c] in upper_case: hasUpper = 1
-        elif password[c] in special_char: hasSpecial = 1
+            currentRepeatingChar = 0
+            lastChr = password[c]
+        if password[c] in numbers: hasNumber = 0
+        elif password[c] in lower_case: hasLower = 0
+        elif password[c] in upper_case: hasUpper = 0
+        elif password[c] in special_char: hasSpecial = 0
     #we return the maximum number between the:
     #       1: difference between the minimum password length and the number of characters 
     #       2: difference between the number of conditions that need to be met and the ones that are already met
     #       3: the number of times more than 3 characters appear toghether
     max1 = max(6-password_length,password_length-20)
-    max2 = max(max1,maxRepeatingChars)
-    return max(max2, 4 - hasNumber - hasSpecial - hasLower - hasUpper)
+    return max(max1, hasNumber + hasSpecial + hasLower + hasUpper + maxRepeatingChars)
 #read the number of characters in the password and the pa
 if __name__ == "__main__":
     password = input().strip()
